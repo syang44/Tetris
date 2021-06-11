535d902687f0575c3aef403b64eb76d0344202f3
Stephanie Yang <81589347+syang44@users.noreply.github.com>
2021-06-10 18:48:11 -0700

    Implemented start/stop button, wrapped everything in DOMContentLoaded

--- app.js
+++ app.js
@@ -18,10 +18,11 @@ from './globals.js';
 //var squares = Array.from(document.querySelectorAll('.grid div'));
 //console.log(squares);
 
-let nextRandom = 0;
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    //let nextRandom = 0;
+    let nextRandom = 0;
+    let timerId;
     //initial starting width and height
     addDiv(width, height);
 
@@ -36,243 +37,256 @@ document.addEventListener('DOMContentLoaded', () => {
     grid = document.querySelectorAll('.grid');
     squares = Array.from(document.querySelectorAll('.grid div'));
     console.log(squares, "eventListener");
-});
 
-//Update block placements when width changes
-function updateBlocks() {
-    setlBlock(width);
-    setzBlock(width);
-    setiBlock(width);
-    settBlock(width);
-    setoBlock(width);
-    setiBlock(width);
-    setBlocks();
-}
 
-//Add divs for grid size dynamically according to user input
-function addDiv(width, height) {
+    //Update block placements when width changes
+    function updateBlocks() {
+        setlBlock(width);
+        setzBlock(width);
+        setiBlock(width);
+        settBlock(width);
+        setoBlock(width);
+        setiBlock(width);
+        setBlocks();
+    }
+
+    //Add divs for grid size dynamically according to user input
+    function addDiv(width, height) {
+        //remove previous divs
+        var elem = document.getElementById('block');
+        while (elem != null) {
+            elem.parentNode.removeChild(elem);
+            elem = document.getElementById('block');
+        }
+
+        var elem = document.getElementById('taken');
+        while (elem != null) {
+            elem.parentNode.removeChild(elem);
+            elem = document.getElementById('taken');
+        }
+
+        //dynamically create new divs
+        let size = width * height;
+        for (let i = 0; i < size; i++) {
+            let newDiv = document.createElement('div');
+            newDiv.id = 'block';
+            //append each div to parent grid
+            document.getElementById('grid').appendChild(newDiv);
+        }
+
+        //add "taken" divs
+        for (let i = 0; i < width; i++) {
+            let newDiv = document.createElement('div');
+            newDiv.id = 'taken';
+            newDiv.classList.add('taken');
+            document.getElementById('grid').appendChild(newDiv);
+        }
+
+        //MAY NEED TO MOVE THIS TO GLOBAL
+        //create array of square on the grid
+        //grid = document.querySelectorAll('.grid');
+        //squares = Array.from(document.querySelectorAll('.grid div'));
+        console.log(squares, "addDiv");
+        //need to update block placement when grid changes
+        updateBlocks();
+    }
+
+    //Update current width/height of grid
+    function updateGrid() {
+        //Get new value of width and height
+        setWidth(document.getElementById("widthInput").value);
+        document.getElementById("width").innerHTML = width;
+        setHeight(document.getElementById("heightInput").value);
+        document.getElementById("height").innerHTML = height;
+
+        //create divs for each cell
+        addDiv(width, height);
+
+        //set size of parent grid
+        let parentWidth = width * 20;
+        let parentHeight = height * 20;
+        document.documentElement.style.setProperty(`--width`, parentWidth + 'px');
+        document.documentElement.style.setProperty(`--height`, parentHeight + 'px');
+
+    }
+
     //remove previous divs
-    var elem = document.getElementById('block');
+    /*var elem = document.getElementById('block');
     while (elem != null) {
         elem.parentNode.removeChild(elem);
         elem = document.getElementById('block');
-    }
-
-    var elem = document.getElementById('taken');
-    while (elem != null) {
-        elem.parentNode.removeChild(elem);
-        elem = document.getElementById('taken');
-    }
+    }*/
 
     //dynamically create new divs
-    let size = width * height;
+    /*let size = width * height;
     for (let i = 0; i < size; i++) {
         let newDiv = document.createElement('div');
-        newDiv.id = 'block';
+        //newDiv.id = 'block'; 
         //append each div to parent grid
         document.getElementById('grid').appendChild(newDiv);
-    }
 
-    //add "taken" divs
-    for (let i = 0; i < width; i++) {
-        let newDiv = document.createElement('div');
-        newDiv.id = 'taken';
-        newDiv.classList.add('taken');
-        document.getElementById('grid').appendChild(newDiv);
-    }
-
+    }*/
     //MAY NEED TO MOVE THIS TO GLOBAL
     //create array of square on the grid
-    //grid = document.querySelectorAll('.grid');
-    //squares = Array.from(document.querySelectorAll('.grid div'));
-    console.log(squares, "addDiv");
+    var grid = document.querySelectorAll('.grid');
+    var squares = Array.from(document.querySelectorAll('.grid div'));
+
     //need to update block placement when grid changes
     updateBlocks();
-}
 
-//Update current width/height of grid
-function updateGrid() {
-    //Get new value of width and height
-    setWidth(document.getElementById("widthInput").value);
-    document.getElementById("width").innerHTML = width;
-    setHeight(document.getElementById("heightInput").value);
-    document.getElementById("height").innerHTML = height;
+    let currentPosition = 4;
+    let currentRotation = 0;
 
-    //create divs for each cell
-    addDiv(width, height);
+    //randomly select a tetromino and its rotation
+    let random = Math.floor(Math.random() * blocks.length)
+    let currentTetromino = blocks[random][currentRotation];
 
-    //set size of parent grid
-    let parentWidth = width * 20;
-    let parentHeight = height * 20;
-    document.documentElement.style.setProperty(`--width`, parentWidth + 'px');
-    document.documentElement.style.setProperty(`--height`, parentHeight + 'px');
-
-}
-
-//remove previous divs
-/*var elem = document.getElementById('block');
-while (elem != null) {
-    elem.parentNode.removeChild(elem);
-    elem = document.getElementById('block');
-}*/
-
-//dynamically create new divs
-/*let size = width * height;
-for (let i = 0; i < size; i++) {
-    let newDiv = document.createElement('div');
-    //newDiv.id = 'block'; 
-    //append each div to parent grid
-    document.getElementById('grid').appendChild(newDiv);
-
-}*/
-//MAY NEED TO MOVE THIS TO GLOBAL
-//create array of square on the grid
-var grid = document.querySelectorAll('.grid');
-var squares = Array.from(document.querySelectorAll('.grid div'));
-
-//need to update block placement when grid changes
-updateBlocks();
-
-let currentPosition = 4;
-let currentRotation = 0;
-
-//randomly select a tetromino and its rotation
-let random = Math.floor(Math.random() * blocks.length)
-let currentTetromino = blocks[random][currentRotation];
-
-//Draw the tetromino
-function draw() {
-    //Add class to each of the squares
-    currentTetromino.forEach(index => {
-        squares[currentPosition + index].classList.add('tetromino');
-        console.log("test");
-    });
-}
-
-//undraw the tetronimo
-function undraw() {
-    currentTetromino.forEach(index => {
-        squares[currentPosition + index].classList.remove('tetromino')
-    })
-}
-
-//draw();
-
-//make the tetromino move down every second
-let timerId = setInterval(moveDown, 1000);
-
-//assign functions to keyCodes
-function control(e) {
-    switch (e.keyCode) {
-        case 37:
-            moveLeft();
-            break;
-        case 38:
-            rotate();
-            break;
-        case 39:
-            moveRight();
-            break;
-        case 40:
-            moveDown();
-            break;
+    //Draw the tetromino
+    function draw() {
+        //Add class to each of the squares
+        currentTetromino.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino');
+        });
     }
-}
 
-document.addEventListener('keydown', control);
+    //undraw the tetronimo
+    function undraw() {
+        currentTetromino.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
 
-//move tetromino down
-function moveDown() {
-    undraw();
-    currentPosition += width;
-    draw();
-    freeze();
-}
+    //draw();
 
-//stop tetromino from going down if it hits something 
-function freeze() {
-    if (currentTetromino.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-        currentTetromino.forEach(index => squares[currentPosition + index].classList.add('taken'));
-        //start a new tetromino falling
-        random = nextRandom;
-        nextRandom = Math.floor(Math.random() * blocks.length);
-        currentTetromino = blocks[random][currentRotation];
-        currentPosition = 4;
+    //make the tetromino move down every second
+    //timerId = setInterval(moveDown, 1000);
+
+    //assign functions to keyCodes
+    function control(e) {
+        switch (e.keyCode) {
+            case 37:
+                moveLeft();
+                break;
+            case 38:
+                rotate();
+                break;
+            case 39:
+                moveRight();
+                break;
+            case 40:
+                moveDown();
+                break;
+        }
+    }
+
+    document.addEventListener('keydown', control);
+
+    //move tetromino down
+    function moveDown() {
+        undraw();
+        currentPosition += width;
         draw();
-        displayShape();
-    }
-}
-
-//move tetromino left unless it's at the edge or if theres a block
-function moveLeft() {
-    undraw();
-
-    //check if tetronimo is at the left edge
-    const isAtLeftEdge = currentTetromino.some(index => (currentPosition + index) % width === 0);
-
-    //if not at left edge, move left by 1
-    if (!isAtLeftEdge) currentPosition -= 1;
-
-    //stop tetromino from going left if it is going into spaces that area already occupied
-    if (currentTetromino.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-        currentPosition += 1;
+        freeze();
     }
 
-    draw();
-}
-
-//move tetromino right unless it's at the edge or if theres a block
-function moveRight() {
-    undraw();
-
-    //check if tetronimo is at the right edge
-    const isAtRightEdge = currentTetromino.some(index => (currentPosition + index) % width === width - 1);
-
-    //if not at right edge, move right by 1
-    if (!isAtRightEdge) currentPosition += 1;
-
-    //stop tetromino from going right if it is going into spaces that area already occupied
-    if (currentTetromino.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-        currentPosition -= 1;
+    //stop tetromino from going down if it hits something 
+    function freeze() {
+        if (currentTetromino.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            currentTetromino.forEach(index => squares[currentPosition + index].classList.add('taken'));
+            //start a new tetromino falling
+            random = nextRandom;
+            nextRandom = Math.floor(Math.random() * blocks.length);
+            currentTetromino = blocks[random][currentRotation];
+            currentPosition = 4;
+            draw();
+            displayShape();
+        }
     }
 
-    draw();
-}
+    //move tetromino left unless it's at the edge or if theres a block
+    function moveLeft() {
+        undraw();
 
-//rotate tetromino
-function rotate() {
-    undraw();
-    currentRotation++;
-    //if current rotation is 4, go back to original rotation
-    if (currentRotation === currentTetromino.length) {
-        currentRotation = 0;
+        //check if tetronimo is at the left edge
+        const isAtLeftEdge = currentTetromino.some(index => (currentPosition + index) % width === 0);
+
+        //if not at left edge, move left by 1
+        if (!isAtLeftEdge) currentPosition -= 1;
+
+        //stop tetromino from going left if it is going into spaces that area already occupied
+        if (currentTetromino.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition += 1;
+        }
+
+        draw();
     }
-    currentTetromino = blocks[random][currentRotation];
-    draw();
-}
 
-//display next tetromino in miniGrid
-const displaySquares = document.querySelectorAll('.miniGrid div');
-const displayWidth = 4;
-let displayIndex = 0;
+    //move tetromino right unless it's at the edge or if theres a block
+    function moveRight() {
+        undraw();
 
-//the tetrominos without rotations
-const nextTetrominos = [
-    [1, displayWidth + 1, displayWidth * 2 + 1, 2], //L Tetromino
-    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], //Z Tetromino
-    [1, displayWidth, displayWidth + 1, displayWidth + 2], //T Tetromino
-    [0, 1, displayWidth, displayWidth + 1], //O Tetromino
-    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1] //I Tetromino
-]
+        //check if tetronimo is at the right edge
+        const isAtRightEdge = currentTetromino.some(index => (currentPosition + index) % width === width - 1);
 
-//dispaly next shape in miniGrid
-function displayShape() {
-    //remove tetromino from entire grid
-    displaySquares.forEach(square => {
-        square.classList.remove('tetromino');
+        //if not at right edge, move right by 1
+        if (!isAtRightEdge) currentPosition += 1;
+
+        //stop tetromino from going right if it is going into spaces that area already occupied
+        if (currentTetromino.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -= 1;
+        }
+
+        draw();
+    }
+
+    //rotate tetromino
+    function rotate() {
+        undraw();
+        currentRotation++;
+        //if current rotation is 4, go back to original rotation
+        if (currentRotation === currentTetromino.length) {
+            currentRotation = 0;
+        }
+        currentTetromino = blocks[random][currentRotation];
+        draw();
+    }
+
+    //display next tetromino in miniGrid
+    const displaySquares = document.querySelectorAll('.miniGrid div');
+    const displayWidth = 4;
+    let displayIndex = 0;
+
+    //the tetrominos without rotations
+    const nextTetrominos = [
+        [1, displayWidth + 1, displayWidth * 2 + 1, 2], //L Tetromino
+        [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], //Z Tetromino
+        [1, displayWidth, displayWidth + 1, displayWidth + 2], //T Tetromino
+        [0, 1, displayWidth, displayWidth + 1], //O Tetromino
+        [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1] //I Tetromino
+    ]
+
+    //dispaly next shape in miniGrid
+    function displayShape() {
+        //remove tetromino from entire grid
+        displaySquares.forEach(square => {
+            square.classList.remove('tetromino');
+        });
+
+        nextTetrominos[nextRandom].forEach(index => {
+            displaySquares[displayIndex + index].classList.add('tetromino');
+        })
+    }
+
+    //add functionality to the button
+    startButton.addEventListener(('click'), () => {
+        if (timerId) {
+            clearInterval(timerId);
+            timerId = null;
+        } else {
+            draw();
+            timerId = setInterval(moveDown, 1000);
+            nextRandom = Math.floor(Math.random() * blocks.length);
+        }
     });
 
-    nextTetrominos[nextRandom].forEach(index => {
-        displaySquares[displayIndex + index].classList.add('tetromino');
-    })
-}
+});
